95cfd9d144bf32cd79707c47302616f72b299ba1
Ronita Chhim <ronitachhim@mbp-de-ronita.lan>
2022-07-29 15:39:56 +0200

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import { link } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    constfetchData = async () => {
+      const response = await axios.get(
+        "https://lereacteur-vinted-api.herokuapp.com/offers"
+      );
+      console.log(response.data);
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return <p>Loading</p>;
+};
+export default Home;
---
+++ src/pages/Offer.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+const Offer = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
+};
+
+export default Offer;
